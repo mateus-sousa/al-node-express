134a1032bee939cd73d4c372cc93451412356e75
Mateus Silva <mateussilva@ntopus.com.br>
2023-04-26 21:45:40 -0300

    up

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://mateussousa157:Mp92699004@cluster0.mom8hib.mongodb.net/alura-node")
+mongoose.connect("mongodb+srv://mateussousa157:213asd2@cluster0.mom8hib.mongodb.net/alura-node")
 
 let db = mongoose.connection
 
